bf56b6f71a9c758bd2c54489dc63ff73db53a0e2
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-11-03 09:53:24 +0700

    add route for api update qty
    add route for api update qty in cart routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -4,6 +4,7 @@ import {
     addItemToCart,
     removeItemFromCart,
     removeAllItemFromCart,
+    updateQty,
 } from '../controllers/cartController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -13,5 +14,6 @@ router.get('/', protect, getMyCart)
 router.put('/add-to-cart/:id', protect, addItemToCart)
 router.put('/remove-all-item-cart/', protect, removeAllItemFromCart)
 router.put('/remove-item-cart/:id', protect, removeItemFromCart)
+router.put('/qty-update/:id', protect, updateQty)
 
 export default router
