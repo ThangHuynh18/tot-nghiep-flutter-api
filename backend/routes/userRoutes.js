65356d80ed719b2c54f1c87cee06a26828a848ae
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-11-07 11:15:22 +0700

    add route for api dashboard
    add route for api dashboard in user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,6 +10,7 @@ import {
   updateUser,
   changePassword,
   totalUser,
+  dashboard,
 } from '../controllers/userControllers.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 import cors from 'cors'
@@ -22,6 +23,7 @@ router.get('/profile', protect, getUserProfile)
 router.put('/profile', protect, updateUserProfile)
 router.put('/change-password', protect, changePassword)
 router.get('/total', protect, admin, totalUser)
+router.get('/dashboard', protect, admin, dashboard)
 router.get('/:id', protect, admin, getUserById)
 router.put('/:id', protect, admin, updateUser)
 router.delete('/:id', protect, admin, deleteUser)
