2f41917becb3a3f9b3ba2f5bf298c3dc1b3437aa
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-11-12 21:58:21 +0700

    add route for api get all order supplier by status
    add route for api get all order supplier by status in order supplier routes

--- backend/routes/orderSupplierRoutes.js
+++ backend/routes/orderSupplierRoutes.js
@@ -4,6 +4,7 @@ import {
   getOrderSupplierById,
   getOrderSuppliers,
   updateOrderSupplierStatus,
+  getAllOrderSupplierByStatus,
 } from '../controllers/orderSupplierControllers.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -13,5 +14,6 @@ router.get('/', protect, admin, getOrderSuppliers)
 router.post('/', protect, admin, addOrderItems)
 router.get('/:id', protect, admin, getOrderSupplierById)
 router.put('/:id/status', protect, admin, updateOrderSupplierStatus)
+router.get('/status/:status', protect, admin, getAllOrderSupplierByStatus)
 
 export default router
