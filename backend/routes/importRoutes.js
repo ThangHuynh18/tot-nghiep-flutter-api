76a6abcfb67a775f27071652f864c4ecd26c9327
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-13 18:28:37 +0700

    Update importRoutes.js

--- backend/routes/importRoutes.js
+++ backend/routes/importRoutes.js
@@ -10,10 +10,15 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.get('/', protect, admin, getImportOrder)
-router.post('/', protect, admin, addImportItems)
-router.get('/:id', protect, admin, getImportOrderById)
-router.put('/:id/status', protect, admin, updateImportOrderStatus)
-router.get('/status/:status', protect, admin, getAllImportByStatus)
+router.get('/', protect, getImportOrder)
+router.post('/', protect, addImportItems)
+router.get('/:id', protect, getImportOrderById)
+router.put('/:id/status', protect, updateImportOrderStatus)
+router.get('/status/:status', protect, getAllImportByStatus)
+// router.get('/', protect, admin, getImportOrder)
+// router.post('/', protect, admin, addImportItems)
+// router.get('/:id', protect, admin, getImportOrderById)
+// router.put('/:id/status', protect, admin, updateImportOrderStatus)
+// router.get('/status/:status', protect, admin, getAllImportByStatus)
 
 export default router
