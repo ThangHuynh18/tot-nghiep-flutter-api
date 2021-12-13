6a5a4b62f99cda9ca1f3517544353ed11e879a1c
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-13 18:30:22 +0700

    Update orderSupplierRoutes.js

--- backend/routes/orderSupplierRoutes.js
+++ backend/routes/orderSupplierRoutes.js
@@ -10,10 +10,15 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.get('/', protect, admin, getOrderSuppliers)
-router.post('/', protect, admin, addOrderItems)
-router.get('/:id', protect, admin, getOrderSupplierById)
-router.put('/:id/status', protect, admin, updateOrderSupplierStatus)
-router.get('/status/:status', protect, admin, getAllOrderSupplierByStatus)
+router.get('/', protect, getOrderSuppliers)
+router.post('/', protect, addOrderItems)
+router.get('/:id', protect, getOrderSupplierById)
+router.put('/:id/status', protect, updateOrderSupplierStatus)
+router.get('/status/:status', protect, getAllOrderSupplierByStatus)
+// router.get('/', protect, admin, getOrderSuppliers)
+// router.post('/', protect, admin, addOrderItems)
+// router.get('/:id', protect, admin, getOrderSupplierById)
+// router.put('/:id/status', protect, admin, updateOrderSupplierStatus)
+// router.get('/status/:status', protect, admin, getAllOrderSupplierByStatus)
 
 export default router
