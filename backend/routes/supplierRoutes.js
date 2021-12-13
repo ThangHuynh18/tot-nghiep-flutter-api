37b08bf2f235858012cf9716ac5d86d6f99ed0d3
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-13 19:39:09 +0700

    Update supplierRoutes.js

--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -10,8 +10,10 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.get('/', protect, admin, getSuppliers)
-router.get('/:id', protect, admin, getSupplierById)
+router.get('/', protect, getSuppliers)
+router.get('/:id', protect, getSupplierById)
+// router.get('/', protect, admin, getSuppliers)
+// router.get('/:id', protect, admin, getSupplierById)
 router.post('/', protect, admin, createSupplier)
 // router.post('/', protect, admin, createBrand)
 router.put('/:id', protect, admin, updateSupplier)
