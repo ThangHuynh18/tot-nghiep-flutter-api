9ea9d5fc69dd5856f9de6d6417b34cf6de59d4d8
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-15 11:47:07 +0700

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -10,6 +10,7 @@ import {
   getTotalOrdersByStatus,
   getOrdersByStatus,
   getAllOrdersByStatus,
+  updateRefundOrder,
 } from '../controllers/orderControllers.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -23,6 +24,7 @@ router.get('/:id', protect, getOrderById)
 router.put('/:id/pay', protect, updateOrderToPaid)
 router.put('/:id/status', protect, updateOrderStatus)
 router.put('/:id/deliver', protect, admin, updateOrderToDelivered)
+router.put('/:id/refund', protect, updateRefundOrder)
 router.get('/myorders/:status', protect, getOrdersByStatus)
 router.get('/status/:status', protect, getAllOrdersByStatus)
 //router.get('/status/:status',protect, admin, getAllOrdersByStatus)
