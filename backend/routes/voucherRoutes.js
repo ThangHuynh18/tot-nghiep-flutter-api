13e9300820f559e1841379d570c619866459338c
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-18 11:19:37 +0700

    Update voucherRoutes.js

--- backend/routes/voucherRoutes.js
+++ backend/routes/voucherRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import {
-    getVouchers, createVoucher, updateVoucher, deleteVoucher,
+    getVouchers, createVoucher, updateVoucher, deleteVoucher, getDiscountVoucher
 } from '../controllers/voucherController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 import cors from 'cors'
@@ -9,6 +9,7 @@ import cors from 'cors'
 
 
 router.get('/', getVouchers)
+router.get('/discount', getDiscountVoucher)
 router.post('/', protect, admin, createVoucher)
 router.put('/:id',protect, admin, updateVoucher)
 router.delete('/:id', protect, admin, deleteVoucher)
