9e1af4c190ba639f16a54deb840de6aa7c0433b1
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-23 18:35:57 +0700

    Update dashboardController.js

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -213,7 +213,141 @@ const orderBetween = asyncHandler(async (req, res, next) => {
   })
 
 
+
+// @desc        Statistic order sell by month
+// @route       GET /api/dashboard/sale
+// @access      Private/Admin
+const saleMonth = asyncHandler(async (req, res, next) => {
+  try {
+    // const date = req.body.date
+    const month = req.body.month;
+//     console.log(
+//       "🚀 ~ file: dashboardController.js ~ line 679 ~ saleMonth ~ month",
+//       month
+//     );
+    const year = req.body.year;
+    var dateEnd;
+    var dateStart;
+
+    if (
+      month == 1 ||
+      month == 3 ||
+      month == 5 ||
+      month == 7 ||
+      month == 8 ||
+      month == 10 ||
+      month == 12
+    ) {
+      dateEnd = new Date(year, month, 1); // 31/12
+      dateStart = new Date(year, month, -29);
+      console.log("stat: " + dateStart.toISOString());
+//       console.log(
+//         "🚀 ~ file: dashboardController.js ~ line 662 ~ saleMonth ~ date",
+//         new Date(dateEnd.toISOString())
+//       );
+      //console.log("dat: " + dateEnd.toDateString());
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+      dateEnd = new Date(year, month, 1); //
+      dateStart = new Date(year, month, -28);
+      console.log("stat: " + dateStart.toISOString());
+//       console.log(
+//         "🚀 ~ file: dashboardController.js ~ line 662 ~ saleMonth ~ date",
+//         new Date(dateEnd.toISOString())
+//       );
+      //console.log("dat: " + dateEnd.toDateString());
+    } else if (month == 2) {
+      dateEnd = new Date(year, month, 1); //
+      dateStart = new Date(year, month, -26);
+      console.log("stat: " + dateStart.toISOString());
+//       console.log(
+//         "🚀 ~ file: dashboardController.js ~ line 662 ~ saleMonth ~ date",
+//         new Date(dateEnd.toISOString())
+//       );
+      //console.log("dat: " + dateEnd.toDateString());
+    }
+
+    // var days = [];
+    // while (date.getMonth() === month - 1) {
+    //     date.setDate(date.getDate() + 1);
+    //     console.log("🚀 ~ file: dashboardController.js ~ line 666 ~ saleMonth ~ date", date)
+    //     days.push((date.getDate()))
+    // }
+    // console.log(
+    //   "🚀 ~ file: dashboardController.js ~ line 687 ~ saleMonth ~ date",
+    //   days
+    // );
+    // console.log("days length: " + days.length);
+    // console.log("🚀 ~ file: dashboardController.js ~ line 675 ~ saleMonth ~ date.getMonth()", date.getMonth())
+    // console.log("date year: " + date.getFullYear());
+
+    // if (date.getMonth() === month - 1) {
+    // console.log("month: "+month +" , year: "+year)
+
+    const orders = await Order.aggregate([
+      // {$project: {_id: 0, _month: {$month: '$createdAt'}}},
+      {
+        $match: {
+          status: "FINISH",
+        },
+      },
+
+      {
+        $match: {
+          createdAt: {
+            // $gt: new Date(date.toString()+ "T00:00:00"),
+            // $lte: new Date(date.toString() + "T23:59:59")
+            //   $gte: new Date(year, month , 1).toISOString(),
+            $gte: new Date(dateStart.toISOString()),
+            $lte: new Date(dateEnd.toISOString()),
+          },
+        },
+      },
+
+      {
+        $project: {
+          totalPrice: 1,
+          orderItems: 1,
+          createdAt: 1,
+        },
+      },
+
+      { $sort: { totalPrice: -1 } },
+      // { $limit: 10 },
+    ]);
+
+    console.log("orders lenth: " + orders.length);
+    var profit;
+    let totalSell = 0;
+    let countOrder = 0;
+    // profit.push({
+    //   countOrder: 0,
+    //   totalSell: 0,
+    // });
+
+    orders.map((order) => {
+      //month = JSON.stringify(order.createdAt).slice(6, 8);
+      totalSell += parseInt(order.totalPrice);
+
+      // profit[month - 1].countOrder++;
+      // profit[month - 1].totalSell += totalSell;
+      //totalSell = 0;
+    });
+    profit = {
+      countOrder: orders.length,
+      totalSell: totalSell,
+    };
+
+    res.status(200).json({ status: "success", data: profit, error: null });
+    // } else res.status(200).json({ status: "failed", data: "", error: null });
+  } catch (error) {
+    res.status(400);
+    throw new Error(`${error}`);
+  }
+});
+
+
 export {
     productBestSeller,
     orderBetween,
+    saleMonth,
 }
