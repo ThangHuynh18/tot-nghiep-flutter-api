1681ac1c3db163664ea99a45f6f852739519c1ba
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-12-23 18:45:04 +0700

    Update dashboardRoutes.js

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -3,6 +3,7 @@ import {
     productBestSeller,
     orderBetween,
     saleMonth,
+    profitMonth,
 } from '../controllers/dashboardController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -11,6 +12,7 @@ const router = express.Router()
 router.put('/order', protect, admin, orderBetween)
 router.put('/product', protect, admin, productBestSeller)
 router.put('/sale', protect, admin, saleMonth)
+router.put('/profit', protect, admin, profitMonth)
 
 
 export default router
